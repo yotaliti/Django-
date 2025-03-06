dee06a32c73a65ec6b0c2a167232e3f24d87f4f8
BeStudent <yotalyt10@gmail.com>
2025-03-06 18:20:18 +0500

    hw2 done

--- homeworks/1_2-requests-templates/1.2-requests-templates/pagination/pagination/urls.py
+++ homeworks/1_2-requests-templates/1.2-requests-templates/pagination/pagination/urls.py
@@ -1,4 +1,4 @@
-"""recipes URL Configuration
+"""pagination URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
-from calculator.views import menu
+from django.urls import path, include
 
 urlpatterns = [
-    path('<str:dish>/', menu, name='recipe')
+    path('', include('stations.urls')),
 ]
